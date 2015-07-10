926e8c8e00a24633fe9cd4655f2b8bd86d7d1ccb
Rafael Testa <rafael@jp7.com.br>
2015-07-10 12:15:14 -0300

    Create welcome page

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,10 +12,7 @@
 */
 
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
+Route::get('/', ['as' => 'welcome.index', 'uses' => 'WelcomeController@index']);
 
 Route::resource('articles', 'ArticlesController');
 Route::resource('authors', 'AuthorsController');
